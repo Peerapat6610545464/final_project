1cb56420f2338ceaf5006ab8bbb3d17fe5c5eebe
Peerapat Seenoi <peerapat9825@gmail.com>
2023-11-27 20:23:22 +0700

    Project outline

---
+++ TODO.md
@@ -0,0 +1,29 @@
+# Final project for 2023's 219114/115 Programming I
+* logged in as a student 
+  - See if there are pending requests to become members of already created projects
+  - Accept or deny the invitation
+  - Create a project and become a lead; must deny all member requests first
+* logged in as a lead :
+  - See project status (pending member, pending advisor, or ready to solicit an advisor)
+  - See and modify project information
+  - Send invitational messages to potential members
+  - Send out requests to a potential advisor; can only do one at a time and after all potential members have accepted or denied the requests
+* logged in as a member :
+  - See project status (like pending member or advisor)
+  - See and modify project information
+  - See who has responded to the requests sent out
+* logged in as a faculty
+  - See request to be a supervisor
+  - Send response denying to serve as an advisor
+  - See details of all the project
+  - Evaluate projects
+* logged in as an advisor
+  - See request to be a advisor
+  - Send accept or deny response
+  - See details of  the project
+* logged in as an admin
+  - See request to be a supervisor
+  - Send accept or deny response (for projects eventually serving as an advisor)
+  - See details of all the project
+  - Evaluate projects
+  - Approve the project
--- database.py
+++ database.py
@@ -1,20 +1,90 @@
 # try wrapping the code below that reads a persons.csv file in a class and make it more general such that it can read in any csv file
 
 import csv, os
+import copy
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-persons = []
-with open(os.path.join(__location__, 'persons.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        persons.append(dict(r))
-print(persons)
+class Read:
+    def __init__(self, name):
+        self.csv = []
+        self.name = name
+
+    def read(self):
+        __location__ = os.path.realpath(
+            os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+        with open(os.path.join(__location__, self.name)) as f:
+            rows = csv.DictReader(f)
+            for r in rows:
+                self.csv.append(dict(r))
+
 
 # add in code for a Database class
+class DB:
+    def __init__(self):
+        self.database = []
+
+    def insert(self, table):
+        self.database.append(table)
+
+    def search(self, table_name):
+        for table in self.database:
+            if table.table_name == table_name:
+                return table
+        return None
+
 
 # add in code for a Table class
+
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
+
+    def insert(self, other):
+        self.table += [other]
+
+    def update(self, user_id, key, value):
+        for i in self.table:
+            if i["ID"] == user_id:
+                self.table[key] = value
+
+    def join(self, other_table, common_key):
+        joined_table = Table(self.table_name + '_joins_' + other_table.table_name, [])
+        for item1 in self.table:
+            for item2 in other_table.table:
+                if item1[common_key] == item2[common_key]:
+                    dict1 = copy.deepcopy(item1)
+                    dict2 = copy.deepcopy(item2)
+                    dict1.update(dict2)
+                    joined_table.table.append(dict1)
+        return joined_table
+
+    def filter(self, condition):
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            if condition(item1):
+                filtered_table.table.append(item1)
+        return filtered_table
+
+    def aggregate(self, function, aggregation_key):
+        temps = []
+        for item1 in self.table:
+            temps.append(float(item1[aggregation_key]))
+        return function(temps)
+
+    def select(self, attributes_list):
+        temps = []
+        for item1 in self.table:
+            dict_temp = {}
+            for key in item1:
+                if key in attributes_list:
+                    dict_temp[key] = item1[key]
+            temps.append(dict_temp)
+        return temps
+
+    def __str__(self):
+        return self.table_name + ':' + str(self.table)
 
 # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
 
