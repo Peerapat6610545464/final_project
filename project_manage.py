6d62b74418f3f2f045a128216e0abe9d0f2286c0
Peerapat Seenoi <peerapat9825@gmail.com>
2023-12-17 15:45:34 +0700

    add admin fucntion

--- project_manage.py
+++ project_manage.py
@@ -43,13 +43,15 @@ def initializing():
 # define a funcion called login
 
 def login():
+    print("-----LOGIN TABLE-----")
     username = input("Username: ")
     password = input("Password: ")
     login_1 = db.search('login')
     for i in login_1.table:
         if username == i['username'] and password == i['password']:
+            print(f"Your log in as {i['role']}")
+            print("----------------------")
             return [i['ID'], i['role']]
-
     # print(login_1.table)
 
 
@@ -97,41 +99,88 @@ initializing()
 val = login()
 
 
-def remove_row(data, row):
-    file = open(f"{data} , w")
-    del file[row - 1]
-    file.close()
+def delete_line(file_delete_, file_delete_number_):
+    with open(f'{file_delete_}.csv') as file_:
+        line = file_.readline()
+        print(line)
+    # if file_delete_number_ <= len(line):
+    #     del line[int(file_delete_number_) - 1]
+    #     with open(f'{file_delete_}.csv', 'w', newline='') as f:
+    #         for i in line:
+    #             file_.write(i)
+    # else:
+    #     print(f"line {file_delete_number_} not in file")
 
 
-def add_project(file_, data):
-    with open(f'{file_}', 'w', newline='') as file:
-        file = csv.writer(file)
-        file.writerow(data)
+def add_line(file_, value):
+    file = db.search(f'{file_}')
+    if file_ == "login":
+        key = {'ID': value[0], 'username': value[1], 'password': value[2],
+               'role': value[3]}
+        file.insert(key)
+    if file_ == "persons":
+        key = {'ID': value[0], 'first': value[1],
+               'last': value[2], 'type': value[4]}
+        file.insert(key)
+        print(key)
+    if file_ == "project":
+        key = {'ProjectID': value[0], 'Title': value[1],
+               'Lead': value[2], 'Member1': value[3],
+               'Member2': value[4], "Advisor": value[5],
+               "Status": value[6]}
+        print(key)
+        file.insert(key)
 
 
 # based on the return value for login, activate the code that performs
 # activities according to the role defined for that person_id
-if val[1] == 'admin':
-    print("---------------------")
-    print("1.Add project")
-    print("2.Delete project")
-    choice = input("Task number: ")
-    if choice == 1:
-        file, data = input("Select file to add row and data too add").split()
-        add_project(file, data)
-    if choice == 2:
-        file, row = input("Select file and row to delete").split()
-        remove_row(file, row)
-if val[1] == 'student':
-    pass
-if val[1] == 'member':
-    pass
-if val[1] == 'lead':
-    pass
-if val[1] == 'faculty':
-    pass
-if val[1] == 'advisor':
-    pass
+while True:
+    if val[1] == 'admin':
+        print("---------------------")
+        print("1.Add project")
+        print("2.Delete project")
+        choice = input("Task number(q to quit): ")
+        if choice == "q":
+            break
+        if choice == "1":
+            file_add = input("File: ")
+            if file_add == "persons":
+                ID = input("ID: ")
+                first = input("first name: ")
+                last = input("last name: ")
+                type_ = input("type: ")
+                add_line(file_add, [ID, first, last, type_])
+            if file_add == "login":
+                ID = input("ID: ")
+                first = input("First name: ")
+                last = input("Last name: ")
+                type_ = input("Role: ")
+                add_line(file_add, [ID, first, last, type_])
+            if file_add == "project":
+                ID = input("ProjectID: ")
+                Title = input("Title: ")
+                Lead = input("Lead: ")
+                Member1 = input("Member1: ")
+                Member2 = input("Member1: ")
+                Advisor = input("Advisor: ")
+                Status = input("Status: ")
+                add_line(file_add,
+                         [ID, Title, Lead, Member1, Member2, Advisor, Status])
+            print("File has been added")
+        if choice == "2":
+            file_delete = input("File: ")
+            file_delete_number = int(input("Line: "))
+            delete_line(file_delete, file_delete_number)
+    if val[1] == 'student':
+        pass
+    if val[1] == 'member':
+        pass
+    if val[1] == 'lead':
+        pass
+    if val[1] == 'faculty':
+        pass
+    if val[1] == 'advisor':
+        pass
 
 # if val[1] = 'admin':
 # see and do admin related activities
--- role.py
+++
@@ -1,4 +0,0 @@
-def remove_row(data, row):
-    file = open(f"{data} , w")
-    del file[row-1]
-    file.close()
